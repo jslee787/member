ab4639cd9ea4d61223e1133da526c03876e0724a
jslee787 <wltntodrkr@naver.com>
2021-12-08 12:51:09 +0900

    member 사이트

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sqlite3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "asdf"   #암호키 발행
 
 def getconn():
     conn = sqlite3.connect('./memberdb.db')
@@ -31,5 +32,55 @@ def member_view(id):    #mid를 경로로 설정하고 매개변수로 넘겨줌
     rs = cur.fetchone() #해당 1개의 자료 반환받음
     conn.close()
     return render_template('member_view.html', rs = rs)
+
+@app.route('/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # 자료 수집
+        id = request.form['mid']
+        pwd = request.form['passwd']
+        name = request.form['name']
+        age = request.form['age']
+        date = request.form['regDate']
+
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "INSERT INTO member VALUES ('%s', '%s', '%s', '%s', '%s')"\
+                % (id, pwd, name, age, date)
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+        return redirect(url_for('memberlist'))  #url 경로로 이동
+
+    else:
+        return render_template('register.html') #GET방식
+
+@app.route("/login/", methods = ['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        # 자료 전달받음
+        id = request.form['mid']
+        pwd = request.form['passwd']
+
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "SELECT * FROM member WHERE mid = '%s' AND passwd = '%s' " % (id, pwd)
+        cur.execute(sql)
+        rs = cur.fetchone() #db에서 일치하는 데이터 가져옴
+        conn.close()
+        if rs:
+            session['userID'] = id
+            return redirect(url_for('index'))
+        else:
+            error = "아이디나 비밀번호가 일치하지 않습니다."
+            return render_template('login.html', error=error)
+
+    else:
+        return render_template('login.html')
+
+@app.route('/logout/')
+def logout():
+    session.pop('userID')   #세션 삭제
+    return redirect(url_for('index'))
 
 app.run(debug=True)
--- tbl_member.py
+++ tbl_member.py
@@ -25,7 +25,7 @@ def insert_member():
     conn = getconn()
     cur = conn.cursor()
     sql = "INSERT INTO member(mid, passwd, name, age) VALUES (?, ?, ?, ?)"
-    cur.execute(sql, ('10002', 'm1234', '팥쥐', 19))
+    cur.execute(sql, ('20001', 'm1234', '흥부', 35))
     conn.commit()
     print("멤버 추가")
     conn.close()
@@ -36,9 +36,9 @@ def select_member():
     sql = "SELECT * FROM member"
     cur.execute(sql)
     rs = cur.fetchall()     #db에서 반환된 자료
-    print(rs)
+    #print(rs)
     for i in rs:
-        print(i[0])
+        print(i)
     conn.close()
 
 #create_table()  #호출
